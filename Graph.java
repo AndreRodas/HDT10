b14ab114197d12043b9e4d025cb7385394a47a18
YosemiteNoe <mel14413@uvg.edu.gt>
2015-11-08 23:10:23 -0600

    versión 2

--- Graph.java
+++ Graph.java
@@ -1,40 +1,73 @@
+/**
+*Universidad del Valle de Guatemala
+*Algoritmos y Estructura de Datos
+*Seccion: 10
+*Andre Rodas - carnet 14395
+*Yosemite Melendez - carnet: 14413
+*08/11/2015
+*Hoja de Trabajo 10
+**/
+
 import java.util.ArrayList;
 
+
+/**
+*Esta es la clase que hace al grafo y lo crea
+*a partir de un arreglo de Edges. Hay varios vértices
+*y distancias para llegar de un vértice a otro.
+**/
 public class Graph {
     
-    private ArrayList pesos;
+    private ArrayList distancias;
     private ArrayList vertices;
     private double[][] adyacencia;
     private String[][] recorrido;
-
++	
+	/**
+	 *	 este es el constructor de objetos para crear un
+	 * arreglo de distnacias y uno de vértices	
+	 *
+	 **/
     public Graph(){
      
-        pesos = new ArrayList<Edge>();
+        distancias = new ArrayList<Edge>();
         vertices = new ArrayList<Vertex>();
     }
-     
+	     
+	/**
+	* Método que se utiliza para agregar una arista
+	*
+	**/
     public void addVertex(String name){
         vertices.add(new Vertex(name));
     }
     
+    /**
+	* Método que se utiliza para remover una arista
+	*
+	**/
 	public boolean removeEdge(String origen, String destino){
         Edge aux;
         Vertex ver1;
         Vertex ver2;
-        for (int x=0;x<pesos.size();x++){
-            aux=(Edge)pesos.get(x);
+        for (int x=0;x<distancias.size();x++){
+            aux=(Edge)distancias.get(x);
             ver1=aux.getOrigen();
             ver2=aux.getDestino();
             if((ver1.getNombre().equals(origen)&&(ver2.getNombre().equals(destino)))){
-                pesos.remove(x);
+                distancias.remove(x);
                 return true;
             }
         }
         return false;
     }
     
-   
-    public void addEdge(String origen, String destino, double peso) throws Exception{
+	/**
+	* Método que se utiliza para agregar un vértice
+	* @param: el origen, el destino de llegada y la distancia entre ambos
+	*
+	**/
+    public void addEdge(String origen, String destino, double distancia) throws Exception{
         if (vertices.isEmpty()){
             throw new Exception("No hay vertices");
         }
@@ -55,27 +88,38 @@ public class Graph {
             }
            
         }
-        Edge arista =new Edge((Vertex)vertices.get(indice1),(Vertex)vertices.get(indice2),peso);
-        pesos.add(arista);
+        Edge arista =new Edge((Vertex)vertices.get(indice1),(Vertex)vertices.get(indice2),distancia);
+        distancias.add(arista);
        
     }
 	
-	 public double getPeso(String origen, String destino){
+    /**
+	* Método que se utiliza para obtener la distancia entre dos vértices
+	* @param origen, de dónde sale
+	* @param destino, hacia donde va
+	*
+	**/
+	 public double getdistancia(String origen, String destino){
         if(origen.equals(destino)){
             return 0.0;
         }
         Edge aux = new Edge(new Vertex(origen),new Vertex(destino),6666.6);
         
-        for (int x=0;x<pesos.size();x++){
-            if(aux.equals((Edge)pesos.get(x))){
-                aux=(Edge)pesos.get(x);
-                return aux.getPeso();
+        for (int x=0;x<distancias.size();x++){
+            if(aux.equals((Edge)distancias.get(x))){
+                aux=(Edge)distancias.get(x);
+                return aux.getdistancia();
             }
         }
         
         return 6666.6;
     } 
    
+	 /**
+	* Método que se utiliza para construir el grafo 
+	* como matriz
+	*
+	**/
     public void crearMatriz(){
         adyacencia= new double[vertices.size()][vertices.size()];
         recorrido = new String[vertices.size()][vertices.size()];
@@ -83,11 +127,15 @@ public class Graph {
         for (int x=0;x<vertices.size();x++){
             for(int j=0;j<vertices.size();j++){
                 recorrido[x][j]=((Vertex)vertices.get(j)).getNombre();
-                adyacencia[x][j]=getPeso(((Vertex)vertices.get(x)).getNombre(),((Vertex)vertices.get(j)).getNombre());
+                adyacencia[x][j]=getdistancia(((Vertex)vertices.get(x)).getNombre(),((Vertex)vertices.get(j)).getNombre());
             }
         }
     }   
-   
+    
+    
+	 /**
+	* Método que se utiliza para mostrar matriz
+	**/
     public void imprimirMatriz(){
         System.out.println("Longitud del viaje");
         String cadena="\t\t";
@@ -119,13 +167,19 @@ public class Graph {
         }
     }
         
+    
+	/**
+	* Método con el cual se utiliza el algoritno de Floyd
+	* con este se calcula la distancia más corta que deberá
+	* recorrer
+	**/
     public void algoritmoFloyd(){
        
         double val1;
         double val2;
         for(int i=0;i<vertices.size();i++){
            
-            for(int x=0;x<vertices.size();x++){
+            for(int x=0;x<vertices.size();x++){qu
                 for(int j=0;j<vertices.size();j++){
                     val1=adyacencia[x][j];
                     val2=adyacencia[x][i]+adyacencia[i][j];
@@ -161,13 +215,17 @@ public class Graph {
         return "El centro del grafo es: "+((Vertex)vertices.get(centro)).getNombre();
     }
     
-   
+    
+	 /**
+	* Método que se utiliza para obtener el recorrido que se 
+	* tiene que recorrer y la distancia a recorrer
+	**/
     public String getRecorrido(String origen, String destino){
         String aux1=origen;
         String aux2=destino;
         String aux3="";
         String camino="";
-        double peso =0;
+        double distancia =0;
         int index1=0;
         int index2=0;
         for(int x=0;x<vertices.size();x++){
@@ -178,8 +236,8 @@ public class Graph {
                     index2=x;
                 }
             }
-        peso=adyacencia[index1][index2];
-        if(peso==1000.0){
+        distancia=adyacencia[index1][index2];
+        if(distancia==1000.0){
             return "No hay forma de llegar hasta la ciudad";
         }
         do{
@@ -199,7 +257,7 @@ public class Graph {
         StringBuffer aux = new StringBuffer(camino);
         aux.deleteCharAt(aux.length()-1);
         aux.reverse();
-        return "El Recorrido es: "+aux+" y la distancia hacia la ciudad es de "+peso+" KM";
+        return "El Recorrido es: "+aux+" y la distancia hacia la ciudad es de "+distancia+" KM";
     }
    
 }
